14095c3735e0bdd31f475dffbefeea93cffac3fe
zhouruxin <534418624@qq.com>
2021-03-05 13:56:09 +0800

    闰年判断

---
+++ 闰年判断.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class 闰年判断 {
+    public static void main(String[] args) {
+       Scanner scan = new Scanner(System.in);
+       int year = scan.nextInt();
+       if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)){
+           System.out.println(year+"是闰年");
+       }else{
+           System.out.println(year+"不是闰年");
+       }
+    }
+    
+}
