210d2e30d822f09707b4516bec23538c6bb51b66
zhourx1129 <534418624@qq.com>
2021-07-15 14:20:09 +0800

    '翻转字符串'

---
+++ FlipString.java
@@ -0,0 +1,26 @@
+/**
+ *
+ * 编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 char[] 的形式给出。
+ * 不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。
+ * 你可以假设数组中的所有字符都是 ASCII 码表中的可打印字符。
+ */
+class Solution {
+    public void reverseString(char[] s) {
+        int left = 0,right = s.length-1;
+        while(left < right){
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
+public class FlipString {
+    public static void main(String[] args) {
+        Solution solution=new Solution();
+        char[] s = {'h','e','l','l','o'};
+        solution.reverseString(s);
+        System.out.println(s);
+    }
+}
